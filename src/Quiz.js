9c0ee959ef382a194fd79c5321a65df75a66d005
patfb <>
2019-08-08 18:24:02 -0400

    resetting the quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,6 +13,11 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -20,7 +25,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
